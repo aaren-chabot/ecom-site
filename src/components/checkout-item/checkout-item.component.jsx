970f0e3274fa7aaf7b17747205694c80ac93c1e9
Aaren Chabot <chabot.aaren@gmail.com>
2019-09-01 16:44:27 -0400

    add REMOVE_ITEM to cart action, type, reducer. Create cart util for lower quantity or remove if 1 wire up checkout item dipatch events

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { clearItemFromCart } from "../../redux/cart/cart.actions";
+import { clearItemFromCart, addItem, removeItem } from "../../redux/cart/cart.actions";
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
 	const { name, imageUrl, price, quantity } = cartItem;
 	return (
 		<div className='checkout-item'>
@@ -13,7 +13,19 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 				<img src={imageUrl} alt="item"/>
 			</div>
 			<span className='name'>{name}</span>
-			<span className='quantity'>{quantity}</span>
+			<span className='quantity'>
+				<div
+					onClick={() => removeItem(cartItem)}
+					className='arrow'>
+					&#10094;
+				</div>
+				<span className='value'>{quantity}</span>
+				<div
+					onClick={() => addItem(cartItem)}
+					className='arrow'>
+					&#10095;
+				</div>
+			</span>
 			<span className='price'>{price}</span>
 			<span className='remove-button' onClick={() => clearItem(cartItem)}>
 				&#10005;
@@ -23,7 +35,9 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 };
 
 const mapDispatchToProps = dispatch => ({
-	clearItem: item => dispatch(clearItemFromCart(item))
+	clearItem: item => dispatch(clearItemFromCart(item)),
+	addItem: item => dispatch(addItem(item)),
+	removeItem: item => dispatch(removeItem(item))
 });
 
 export default connect(
