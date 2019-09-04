178cdf9e74675d0bb9e2ad4d246a94775fcc8fbf
Aaren Chabot <chabot.aaren@gmail.com>
2019-09-03 22:19:05 -0400

    normalize shop data into obj and create selector for collections

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,16 +7,19 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection: { title, items } }) => (
-	<div className='collection-page'>
-		<h2 className='title'>{title}</h2>
-		<div className='items'>
-			{
-				items.map(item => <CollectionItem key={item.id} item={item} />)
-			}
+const CollectionPage = ({ collection }) => {
+	const { title, items } = collection;
+	return (
+		<div className='collection-page'>
+			<h2 className='title'>{title}</h2>
+			<div className='items'>
+				{
+					items.map(item => <CollectionItem key={item.id} item={item} />)
+				}
+			</div>
 		</div>
-	</div>
-);
+	)
+};
 
 const mapStateToProps = (state, ownProps) => ({
 	collection: selectCollection(ownProps.match.params.collectionId)(state)
