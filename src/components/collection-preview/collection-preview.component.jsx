e6d8db22944bd44ec813d705c6f984b5a743383f
Aaren Chabot <chabot.aaren@gmail.com>
2019-09-16 19:15:08 -0400

    update h1 to Link for collection preview comp

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './collection-preview.styles.scss';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items }) => {
+	return (
 	<div className='collection-preview'>
-		<h1 className='title'>{title.toUpperCase()}</h1>
+		<Link className='title' to={`/shop/${title.toLowerCase()}`}>{title.toUpperCase()}</Link>
 		<div className='preview'>
 			{
 				items
@@ -17,6 +19,6 @@ const CollectionPreview = ({ title, items }) => (
 			}
 		</div>
 	</div>
-);
+)};
 
 export default CollectionPreview;
