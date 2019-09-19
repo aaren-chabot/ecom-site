69f2c6c695f8f51461b5b0d9cca2cd0cb8d14735
Aaren Chabot <chabot.aaren@gmail.com>
2019-09-19 15:31:30 -0400

    create shop saga, root saga and replace thunk as a middleware. Also, move async from shop into saga

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,13 +5,13 @@ import { connect } from 'react-redux';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
 class ShopPage extends Component{
 
 	componentDidMount() {
-		const { fetchCollectionsStartAsync } = this.props;
-		fetchCollectionsStartAsync()
+		const { fetchCollectionsStart } = this.props;
+		fetchCollectionsStart()
 	}
 
 	render() {
@@ -34,7 +34,7 @@ class ShopPage extends Component{
 }
 
 const mapDispatchToProps = dispatch => ({
-	fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+	fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
 
 export default connect(
