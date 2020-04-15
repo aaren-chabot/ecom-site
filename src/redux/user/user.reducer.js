bdcc9c6a74866c1dbf9a9901b49ae99ad63ae41d
Aaren Chabot <chabot.aaren@gmail.com>
2020-04-15 13:02:07 -0400

    created user sagas for sign-up component

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -21,11 +21,12 @@ const userReducer = (state = INITIAL_STATE, action) => {
 			};
 		case UserActionTypes.SIGN_OUT_FAILURE:
 		case UserActionTypes.SIGN_IN_FAILURE:
+    case UserActionTypes.SIGN_UP_FAILURE:
 			return {
 				...state,
 				loginStarted: false,
 				errorMessage: action.payload
-			};
+      };
 		default:
 			return state;
 	}
