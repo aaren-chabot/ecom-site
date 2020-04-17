a85f5f41b2257f889645fc208457e92aa044c3f9
Aaren Chabot <chabot.aaren@gmail.com>
2020-04-17 16:08:49 -0400

    menu-item component update to styled-component

--- client/src/components/menu-item/menu-item.component.jsx
+++ client/src/components/menu-item/menu-item.component.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
-import './menu-item.styles.scss';
+import {
+  MenuItemContainer,
+  BackgroundImageContainer,
+  ContentContainer,
+  ContentTitle,
+  ContentSubtitle
+} from './menu-item.styles';
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-	<div
-		onClick={() => history.push(`${match.url}${linkUrl}`)}
-		className={`menu-item ${size}`}
-	>
-		<div
-			className='background-image'
-			style={{ backgroundImage: `url(${imageUrl})` }} />
-		<div className='content'>
-			<h1 className='title'>{title.toUpperCase()}</h1>
-			<span className='subtitle'>SHOP NOW</span>
-		</div>
-	</div>
+  <MenuItemContainer
+    size={size}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <BackgroundImageContainer
+      className='background-image'
+      imageUrl={imageUrl}
+    />
+    <ContentContainer className='content'>
+      <ContentTitle>{title.toUpperCase()}</ContentTitle>
+      <ContentSubtitle>SHOP NOW</ContentSubtitle>
+    </ContentContainer>
+  </MenuItemContainer>
 );
 
 export default withRouter(MenuItem);
