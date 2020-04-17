3945b80577d2e63ca66b23811bcf1ee0ccb9e1de
Aaren Chabot <chabot.aaren@gmail.com>
2020-04-17 16:03:03 -0400

    collection-preview update to styled-component

--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-import './collection-preview.styles.scss';
+import { withRouter } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
-const CollectionPreview = ({ title, items }) => {
-	return (
-	<div className='collection-preview'>
-		<Link className='title' to={`/shop/${title.toLowerCase()}`}>{title.toUpperCase()}</Link>
-		<div className='preview'>
-			{
-				items
-					.filter((item, idx) => idx < 4)
-					.map((item) => (
-						<CollectionItem key={item.id} item={item} />
-				))
-			}
-		</div>
-	</div>
-)};
+import {
+  CollectionPreviewContainer,
+  TitleContainer,
+  PreviewContainer
+} from './collection-preview.styles';
 
-export default CollectionPreview;
+const CollectionPreview = ({ title, items, history, match, routeName }) => (
+  <CollectionPreviewContainer>
+    <TitleContainer onClick={() => history.push(`${match.path}/${routeName}`)}>
+      {title.toUpperCase()}
+    </TitleContainer>
+    <PreviewContainer>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+    </PreviewContainer>
+  </CollectionPreviewContainer>
+);
+
+export default withRouter(CollectionPreview);
