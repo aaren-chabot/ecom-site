840242c5bfa235e7747e22db287fa599a92ccedb
Aaren Chabot <chabot.aaren@gmail.com>
2020-04-17 15:56:56 -0400

    cart-item update to styled-component

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
-import './cart-item.styles.scss';
+import {
+  CartItemContainer,
+  ItemDetailsContainer,
+  CartItemImage
+} from './cart-item.styles';
 
 const CartItem = ({ item: { imageUrl, price, name, quantity }}) => (
-	<div className='cart-item'>
-		<img src={imageUrl} alt="item"/>
-		<div className='item-details'>
+	<CartItemContainer>
+		<CartItemImage src={imageUrl} alt="item"/>
+		<ItemDetailsContainer>
 			<span className='name'>{name}</span>
 			<span className='price'>
 				{quantity} ${price}
 				</span>
-		</div>
-	</div>
+		</ItemDetailsContainer>
+  </CartItemContainer>
+
 );
 
 export default CartItem;
