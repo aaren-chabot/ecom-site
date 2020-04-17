3cc729dcf0bef19f7d2981ac60705248a3bd3198
Aaren Chabot <chabot.aaren@gmail.com>
2020-04-17 16:04:36 -0400

    collections-overview update to styled-component

--- client/src/components/collections-overview/collections-overview.component.jsx
+++ client/src/components/collections-overview/collections-overview.component.jsx
@@ -5,17 +5,16 @@ import { createStructuredSelector } from 'reselect';
 import CollectionPreview from "../collection-preview/collection-preview.component";
 
 import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
+import { CollectionsOverviewContainer } from './collections-overview.styles';
 
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => (
-	<div className='collections-overview'>
-		{
-			collections.map(({id, ...otherCollectionProps}) => (
-				<CollectionPreview key={id} {...otherCollectionProps} />
-			))
-		}
-	</div>
+  <CollectionsOverviewContainer>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </CollectionsOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
