d5867366971a704db2585ef3432fe75d6a6aefcd
Aaren Chabot <chabot.aaren@gmail.com>
2020-04-17 15:59:10 -0400

    checkout-item update to styled-component

--- client/src/components/checkout-item/checkout-item.component.jsx
+++ client/src/components/checkout-item/checkout-item.component.jsx
@@ -1,37 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { clearItemFromCart, addItem, removeItem } from "../../redux/cart/cart.actions";
+import { 
+  clearItemFromCart, 
+  addItem, 
+  removeItem 
+} from "../../redux/cart/cart.actions";
 
-import './checkout-item.styles.scss';
+import {
+  CheckoutItemContainer,
+  ImageContainer,
+  TextContainer,
+  QuantityContainer,
+  RemoveButtonContainer
+} from './checkout-item.styles';
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
-	const { name, imageUrl, price, quantity } = cartItem;
-	return (
-		<div className='checkout-item'>
-			<div className='image-container'>
-				<img src={imageUrl} alt="item"/>
-			</div>
-			<span className='name'>{name}</span>
-			<span className='quantity'>
-				<div
-					onClick={() => removeItem(cartItem)}
-					className='arrow'>
-					&#10094;
-				</div>
-				<span className='value'>{quantity}</span>
-				<div
-					onClick={() => addItem(cartItem)}
-					className='arrow'>
-					&#10095;
-				</div>
-			</span>
-			<span className='price'>{price}</span>
-			<span className='remove-button' onClick={() => clearItem(cartItem)}>
-				&#10005;
-			</span>
-		</div>
-	);
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <CheckoutItemContainer>
+      <ImageContainer>
+        <img src={imageUrl} alt='item' />
+      </ImageContainer>
+      <TextContainer>{name}</TextContainer>
+      <QuantityContainer>
+        <div onClick={() => removeItem(cartItem)}>&#10094;</div>
+        <span>{quantity}</span>
+        <div onClick={() => addItem(cartItem)}>&#10095;</div>
+      </QuantityContainer>
+      <TextContainer>{price}</TextContainer>
+      <RemoveButtonContainer onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </RemoveButtonContainer>
+    </CheckoutItemContainer>
+  );
 };
 
 const mapDispatchToProps = dispatch => ({
