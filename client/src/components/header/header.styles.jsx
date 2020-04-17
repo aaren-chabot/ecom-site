a89fb0fbb1d94eb8ee8e5b211a9d44af5ad3165f
Aaren Chabot <chabot.aaren@gmail.com>
2020-04-17 16:52:05 -0400

    mobile fix for header, home and shop pages

--- client/src/components/header/header.styles.jsx
+++ client/src/components/header/header.styles.jsx
@@ -7,12 +7,23 @@ export const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+
+  @media screen and (max-width: 800px) {
+    height: 60px;
+    padding: 10px;
+    margin-bottom: 20px;
+  }
 `;
 
 export const LogoContainer = styled(Link)`
 	height: 100%;
 	width: 70px;
-	padding: 25px;
+  padding: 25px;
+  
+  @media screen and (max-width: 800px) {
+    width: 50px;
+    padding: 0;
+  }
 `;
 
 export const OptionsContainer = styled.div`
@@ -20,7 +31,11 @@ export const OptionsContainer = styled.div`
 	height: 100%;
 	display: flex;
 	align-items: center;
-	justify-content: flex-end;
+  justify-content: flex-end;
+  
+  @media screen and (max-width: 800px) {
+    width: 80%;
+  }
 `;
 
 export const OptionLink = styled(Link)`
