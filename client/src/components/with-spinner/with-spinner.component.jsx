c7321d076ecc001f2b780e14816a6b46dbcf37c9
Aaren Chabot <chabot.aaren@gmail.com>
2020-04-19 18:22:02 -0400

    implement lazy and Suspense for routes from home and shop. Also, pull spinner in to separate component from with-spinner

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 
-import { SpinnerOverlay, SpinnerContainer } from './with-spinner.styles';
+import Spinner from '../spinner/spinner.component';
 
-const WithSpinner = WrappedComponent => {
-	const spinner = ({ isLoading, ...otherProps }) => {
+const WithSpinner = WrappedComponent => 
+({ isLoading, ...otherProps }) => {
 		return isLoading ? (
-			<SpinnerOverlay>
-				<SpinnerContainer />
-			</SpinnerOverlay>
+			<Spinner />
 		) : (
 			<WrappedComponent {...otherProps} />
 		);
-	};
-	return spinner;
 };
 
 export default WithSpinner;
